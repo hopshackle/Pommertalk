82a78f82ccf2ac325ebc11d53d1469fe0a3c5eb2
hopshackle <james@janigo.co.uk>
2020-08-24 16:06:42 +0100

    Important change

--- src/Test.java
+++ src/Test.java
@@ -14,6 +14,8 @@ public class Test {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello World!");
+
         // Game parameters
         long seed = System.currentTimeMillis();
         int boardSize = Types.BOARD_SIZE;
