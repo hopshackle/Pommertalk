bb1c25aa588a1bd34e4804b4efb2c00003c0bc98
gideon raeburn <g.raeburn@qmul.ac.uk>
2020-09-01 23:56:59 +0100

    GUI updates: Add characters and proposal limit

--- src/Test.java
+++ src/Test.java
@@ -43,9 +43,9 @@ public class Test {
         // Currently heuristic weights are ENEMY DEATH / ALLY SURVIVAL / WWOD / CANKICK / FORCE_BLAST / ALLY_DISTANCE / AMMO
         rheaParams.heuristic_weights = new double[]{0.4, 0.1, 0.1, 0.15, 0.15, 0.01, 0.5};
 
-        players.add(new MCTSPlayer(seed, playerID++, mctsParams, new RandomNegotiator(1)));
+        //players.add(new MCTSPlayer(seed, playerID++, mctsParams, new RandomNegotiator(1)));
         //players.add(new MCTSPlayer(seed, playerID++, mctsParams));
-        //players.add(new HumanPlayer(ki1, playerID++));
+        players.add(new HumanPlayer(ki1, playerID++));
 //        players.add(new SimplePlayer(seed, playerID++));
       players.add(new RHEAPlayer(seed, playerID++, rheaParams, new RandomNegotiator(2)));
 //        players.add(new SimplePlayer(seed, playerID++));
